5329e54bf0d5489d6b4aafd2ec6c8ac39a11d234
pradeep singh <pradeep1991singh@gmail.com>
2018-05-15 04:22:18 +0530

    Fix: Android issue

--- src/image-color-picker.js
+++ src/image-color-picker.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
+import { WebView, Platform } from 'react-native'
 import RNFetchBlob from 'react-native-fetch-blob';
-
+import RNImageColorPicker from 'image-color-picker'
 import { canvasHtml } from './canvas-html';
+
+async function getColor(imagePath) {
+  return RNImageColorPicker.getColor(imagePath)
+}
 
 export default class ImageColorPicker extends Component {
   state = {
@@ -15,15 +19,32 @@ export default class ImageColorPicker extends Component {
 
   getImage = async imageUrl => {
     try {
-      await RNFetchBlob.fetch('GET', imageUrl)
-        .then(res => {
-          this.setState({ imageBlob: res.base64() });
-        })
-        .catch((errorMessage, statusCode) => {
-          this.props.pickerCallback(errorMessage, statusCode);
+
+      const localImage = await RNFetchBlob.config({ fileCache: true }).fetch(
+        'GET',
+        imageUrl
+      )
+
+      // if we are on Android, then use native for Android
+      if (Platform.OS === 'android') {
+        let colors = [];
+        const color = await getColor(localImage.path());
+        color.forEach(c => {
+          colors.push(c.split('-'));
         });
-    } catch (e) {
-      this.props.pickerCallback(e);
+        this.imageColorPickerView.props.onMessage({
+          promise: JSON.stringify({ 'message': 'imageColorPicker', 'payload': colors })
+        });
+        return;
+      }
+
+      const base64EncodedImage = await RNFetchBlob.fs.readFile(
+        localImage.path(),
+        'base64'
+      );
+      this.setState({ imageBlob: base64EncodedImage });
+    } catch (error) {
+      this.props.pickerCallback(error);
     }
   };
 
@@ -32,7 +53,7 @@ export default class ImageColorPicker extends Component {
 
     return (
       <WebView
-        ref="imageColorPickerWebview"
+        ref={imageColorPickerView => (this.imageColorPickerView = imageColorPickerView)}
         source={{ html: canvasHtml(this.state.imageBlob, this.props) }}
         javaScriptEnabled={true}
         onMessage={pickerCallback}
