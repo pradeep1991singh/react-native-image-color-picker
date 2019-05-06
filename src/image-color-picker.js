a0c784a10be11f4810a84e20b89ad3c4ccf7a074
Ash Mishra <a.mishra@nfb.ca>
2019-05-06 12:51:12 -0700

    fixes for Webview 5.8.1

--- src/image-color-picker.js
+++ src/image-color-picker.js
@@ -15,7 +15,8 @@ export default class ImageColorPicker extends Component {
   };
 
   componentWillMount() {
-    this.getImage(this.props.imageUrl);
+    if (this.props.imageUrl)
+      this.getImage(this.props.imageUrl);
   }
 
   getImage = async imageUrl => {
@@ -50,14 +51,15 @@ export default class ImageColorPicker extends Component {
   };
 
   render() {
-    const { imageUrl, pickerCallback, pickerStyle } = this.props;
+    const { pickerCallback, pickerStyle } = this.props;
 
     return (
       <WebView
+        originWhitelist={['*']}
         ref={imageColorPickerView => (this.imageColorPickerView = imageColorPickerView)}
         source={{ html: canvasHtml(this.state.imageBlob, this.props) }}
         javaScriptEnabled={true}
-        onMessage={pickerCallback}
+        onMessage={event => { pickerCallback(event); } }
         style={pickerStyle}
       />
     );
